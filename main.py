58cb5dfd36c7200de98aa72918b15dcc3adc435d
90houlaoheshang <907333918@qq.com>
2024-08-13 13:56:56 +0800

    修复对接fastgpt时，非流式提问可能会回复“0: ”开头的响应

--- main.py
+++ main.py
@@ -1,16 +1,101 @@
-# 这是一个示例 Python 脚本。
+import os
+import ujson
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse, Response
+import httpx
+from loguru import logger
+from urllib.parse import urlparse
+from fastapi.middleware.cors import CORSMiddleware
+from utilities.openai_tool import modify_openai_response
 
-# 按 ⌃R 执行或将其替换为您的代码。
-# 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有HTTP方法
+    allow_headers=["*"],  # 允许所有HTTP头
+)
+# OPENAI_BASE_URL校验
+if not os.environ['OPENAI_BASE_URL'].endswith("/v1"):
+    raise ValueError("OPENAI_BASE_URL must end with /v1")
+TARGET_BASE_URL = os.environ['OPENAI_BASE_URL'][:-3]
+# 全局唯一的 httpx.AsyncClient 实例
+client: None | httpx.AsyncClient = None
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 ⌘F8 切换断点。
+@app.on_event("startup")
+async def startup_event():
+    global client
+    client = httpx.AsyncClient()
+    logger.info("HTTP client initialized")
 
 
-# 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.on_event("shutdown")
+async def shutdown_event():
+    global client
+    await client.aclose()
+    logger.info("HTTP client closed")
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+
+@app.middleware("http")
+async def proxy_middleware(request: Request, call_next):
+    # 构建目标URL
+    url = f"{TARGET_BASE_URL}{request.url.path}"
+    logger.debug(f"target url: {url}")
+    # 获取请求方法
+    method = request.method
+
+    # 获取请求头
+    headers = dict(request.headers)
+    parsed_url = urlparse(url)
+    host = parsed_url.netloc
+    headers["host"] = headers["x-forwarded-host"] = host
+
+    if method == "POST" and "/v1/chat/completions" == request.url.path:
+        data = await request.json()
+        logger.debug(f"{data=}")
+
+        stream_gen = await modify_openai_response(data=data, path=request.url.path, channel="openai")
+
+        if data.get("stream", False):
+            resp = StreamingResponse(stream_gen, media_type="text/event-stream")
+        else:
+            resp = Response(ujson.dumps(stream_gen, ensure_ascii=False), status_code=200,
+                            headers={"content-Type": "application/json"})
+
+        resp.headers["Access-Control-Allow-Origin"] = "*"
+
+        return resp
+    elif ((await request.body()) and (await request.json()).get("stream")) or request.query_params.get("stream"):
+        # 通用流式处理，应该基本没啥用
+        data = await request.json()
+        logger.debug(f"{data=}")
+
+        resp = StreamingResponse(
+            modify_openai_response(data=data, method=method, path=request.url.path, channel="httpx"),
+            media_type="text/event-stream")
+        resp.headers["Access-Control-Allow-Origin"] = "*"
+
+        return resp
+    else:
+        # 其他非流式请求
+        # 获取请求体
+        body = await request.body()
+        # 发送请求到目标服务
+        response = await client.request(
+            method=method,
+            url=url,
+            headers=headers,
+            content=body,
+            params=request.query_params
+        )
+
+        # 构建响应
+        return Response(await response.aread(), status_code=response.status_code, headers=response.headers)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ utilities/openai_tool.py
@@ -0,0 +1,143 @@
+import os
+import pprint
+import secrets
+import string
+from typing import AsyncGenerator, Dict
+from urllib.parse import urljoin
+import httpx
+import ujson
+from loguru import logger
+from urllib.parse import urlparse
+from openai import AsyncOpenAI
+
+API_KEY = os.environ.get("OPENAI_API_KEY")
+# 解析出host
+parsed_url = urlparse(os.environ['OPENAI_BASE_URL'])
+BASE_URL = f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+TIMEOUT = 30
+
+client: None | AsyncOpenAI = None
+
+
+async def modify_openai_response(data: Dict, method: str = "POST", path: str = "", channel: str = "openai") \
+        -> AsyncGenerator[str, None] | Dict:
+    """
+    根据是否流式选择处理路线，以及对响应结果的转换
+
+    :param data: 请求体
+    """
+    if not data.get("stream"):
+        async for chat_completion in _request_openai(data, method, path, channel):
+            chat_completion = chat_completion.to_dict()
+            # 代理fastgpt可能的bug
+            if "choices" in chat_completion and chat_completion["choices"][0]['message']['content'].startswith("0: "):
+                chat_completion["choices"][0]['message']['content'] = \
+                    chat_completion["choices"][0]['message']['content'][3:]
+                return chat_completion
+    else:
+        raise NotImplementedError
+
+
+async def _request_openai(data: Dict,
+                          method: str = "POST",
+                          path: str = "",
+                          channel: str = "openai",
+                          yield_type: str = "str") -> AsyncGenerator[str, None]:
+    """
+    对OpenAI API兼容的服务进行请求
+
+    :param data:
+    :param method:
+    :param path:
+    :param channel: 是使用httpx自己构建请求还是openai库。基本上不会使用httpx
+    :param yield_type: 流式请求时流数据的类型，默认为str，例如“'data: {"id":"cmpl-c93b280ab24846bcbc5f707ac391a5b6","choices":[{"delta":{"content":"\n"},"finish_reason":null,"index":0,"logprobs":null}],"created":1718868916,"model":"Qwen/Qwen2-72B-Instruct-GPTQ-Int4","object":"chat.completion.chunk"}
+
+'”；或者dict，例如“{"id":"cmpl-c93b280ab24846bcbc5f707ac391a5b6","choices":[{"delta":{"content":"\n"},"finish_reason":null,"index":0,"logprobs":null}],"created":1718868916,"model":"Qwen/Qwen2-72B-Instruct-GPTQ-Int4","object":"chat.completion.chunk"}”
+    :return:
+    """
+    if method != "POST":
+        raise NotImplementedError
+
+    global client
+
+    if data['messages'][-1].get('role') == 'tool' and 'name' in data['messages'][-1].keys():  # 工具调用结果汇总
+        # 清理'assistant'的tool_calls
+        for message in data['messages']:
+            if message['role'] == 'assistant':
+                if 'tool_calls' in message:
+                    message.pop('tool_calls')
+                if 'content' not in message:
+                    message['content'] = '我将调用外部工具回答这个问题...'
+        # 清理'tool_choice'
+        if 'tool_choice' in data:
+            data.pop('tool_choice')
+        # 清理'tools'
+        if 'tools' in data:
+            data.pop('tools')
+
+        # 工具调用结果范围
+        tool_res_idxs = []
+        meet_tool_res = False
+        for i in range(len(data['messages']) - 1, -1, -1):
+            if data['messages'][i]['role'] == 'tool':
+                meet_tool_res = True
+                tool_res_idxs.append(i)
+            if meet_tool_res and data['messages'][i]['role'] != 'tool':
+                break
+
+        # 工具调用结果转qwen2格式
+        tool_res = [data['messages'].pop(i) for i in tool_res_idxs]
+        # 应该没有role=tool了
+        if any(m['role'] == 'tool' for m in data['messages']):
+            raise NotImplementedError
+        data['messages'].append({
+            'role': 'assistant',
+            'content': f"""
+        外部工具调用结果：{ujson.dumps(tool_res, ensure_ascii=False)}
+                    """,
+        })
+
+    if channel == "httpx" and data.get("stream"):
+        async with httpx.AsyncClient() as client:
+            async with client.stream(
+                    method,
+                    urljoin(BASE_URL, path),
+                    timeout=httpx.Timeout(TIMEOUT),
+                    headers={
+                        "Authorization": f"Bearer {API_KEY}",
+                    },
+                    json=data,
+            ) as response:
+                response.raise_for_status()
+                async for chunk in response.aiter_text():
+                    c_cache = ''
+                    for c in chunk:
+                        c_cache += c
+                        if c_cache.endswith("\n\n"):
+                            logger.debug(f"{c_cache=}")
+                            yield c_cache
+                            c_cache = ''
+                    else:
+                        if c_cache:
+                            yield c_cache
+    elif channel == "openai" and path == "/v1/chat/completions":
+        client = client or AsyncOpenAI()
+
+        if not data.get("stream"):
+            yield await client.chat.completions.create(**data)
+            return
+
+        stream = await client.chat.completions.create(**data)
+        async for chunk in stream:
+            if yield_type == "str":
+                chunk_s = "data: " + ujson.dumps(chunk.to_dict(), ensure_ascii=False) + "\n\n"
+                logger.debug(f"{chunk_s=}")
+                yield chunk_s
+            elif yield_type == "dict":
+                yield chunk.to_dict()
+            else:
+                raise NotImplementedError
+
+    else:
+        raise NotImplementedError
